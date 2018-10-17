3263452bd6c4686b4e6c23499681d987cfd3211f
Hannah <hannahvanseeters@gmail.com>
2018-10-17 16:09:00 -0700

    basic setup for books controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,36 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+  def index
+    @products = Product.all
+  end
+
+  def show; end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit; end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_product
+    @product = Product.find_by(id: params[:id].to_i)
+
+    if @product.nil?
+      render :notfound, status :not_found
+    end
+  end
+
+  def product_params
+    return params.require(:product).permit(:name, :user_id, :price_in_cents, :category, :quantity, :description)
+  end
 end
