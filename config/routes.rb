202c4f48f12666d6d5444b2f31a7c7337af5ae4d
Xtina <2006peacegypsy@gmail.com>
2018-10-18 16:40:04 -0700

    blargh

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get '/pages/about_us', to: 'pages#show', as: 'about_us'
+  get '/pages/contact_us', to: 'pages#contact_us', as: 'contact_us'
   resources :products
   resources :user
   resources :address
