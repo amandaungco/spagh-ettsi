b8d4576ce9f38c927571a2e1d93bba56f5da40fc
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-18 15:28:18 -0700

    address validations - all tests pass

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,5 +2,12 @@ class Address < ApplicationRecord
   belongs_to :user
   has_one :payment
 
+  validates :user_id, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true , numericality: true
 
 end
