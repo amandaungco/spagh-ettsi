81d04c127602a64c5550b46032a3dc666790e5d9
Xtina <2006peacegypsy@gmail.com>
2018-10-18 11:25:39 -0700

    adding to seeds rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,165 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
-MEDIA_FILE = Rails.root.join('db', 'product_seeds.csv')
-puts "Loading raw product data from #{MEDIA_FILE}"
+USER_FILE = Rails.root.join('db', 'user_seeds.csv')
+puts "Loading raw user data from #{USER_FILE}"
+
+user_failures = []
+CSV.foreach(USER_FILE, :headers => true) do |row|
+  user = User.new(
+    id: row['id'],
+    first_name: row['first_name'],
+    last_name: row['last_name'],
+    email: row['email'],
+    is_a_seller: row['is_a_seller'],
+    uid: row['uid'],
+    provider: row['provider']
+  )
+
+  successful = user.save
+
+  if !successful
+    user_failures << user
+    puts "Failed to save user: #{user.inspect}"
+    puts "#{user.errors.full_messages}"
+  else
+    puts "Created user: #{user.inspect}"
+  end
+end
+
+
+ADDRESSES_FILE = Rails.root.join('db', 'addresses_seeds.csv')
+puts "Loading raw user data from #{ADDRESSES_FILE}"
+
+address_failures = []
+CSV.foreach(ADDRESSES_FILE, :headers => true) do |row|
+  address = Address.new(
+    user_id: row['user_id'],
+    first_name: row['first_name'],
+    street: row['street'],
+    street_2: row['street_2'],
+    city: row['city'],
+    state: row['state'],
+    zip: row['zip'],
+  )
+
+  successful = address.save
+
+  if !successful
+    address_failures << address
+    puts "Failed to save address: #{address.inspect}"
+    puts "#{address.errors.full_messages}"
+  else
+    puts "Created address: #{address.inspect}"
+  end
+end
+
+
+PAYMENTS_FILE = Rails.root.join('db', 'payments_seeds.csv')
+puts "Loading raw user data from #{PAYMENTS_FILE}"
+
+payment_failures = []
+CSV.foreach(PAYMENTS_FILE, :headers => true) do |row|
+  payment = Payment.new(
+    user_id: row['user_id'],
+    address_id: row['card_number'],
+    card_number: row['last_name'],
+    expiration_date: row['expiration_date'],
+    cvv: row['cvv'],
+    card_type: row['card_type']
+  )
+
+  successful = payment.save
+
+  if !successful
+    payment_failures << payment
+    puts "Failed to save payment: #{payment.inspect}"
+    puts "#{payment.errors.full_messages}"
+  else
+    puts "Created payment: #{payment.inspect}"
+  end
+end
+
+
+
+
+PRODUCT_FILE = Rails.root.join('db', 'product_seeds.csv')
+puts "Loading raw product data from #{PRODUCT_FILE}"
 
 product_failures = []
-CSV.foreach(MEDIA_FILE, :headers => true) do |row|
-  product = Product.new
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new(
 
-  product.category = row['category']
-  product.name = row['name']
-  product.user_id = row['user_id']
-  product.price_in_cents = row['price_in_cents']
-  product.description = row['description']
-  product.quantity = row['quantity']
+    category: row['category'],
+    name: row['name'],
+    user_id: row['user_id'],
+    price_in_cents: row['price_in_cents'],
+    description: row['description'],
+    quantity: row['quantity']
+  )
 
   successful = product.save
 
   if !successful
     product_failures << product
     puts "Failed to save product: #{product.inspect}"
+    puts "#{product.errors.full_messages}"
   else
     puts "Created product: #{product.inspect}"
   end
 end
 
-puts "Added #{Product.count} product records"
-puts "#{product_failures.length} products failed to save"
+
+
+ORDER_FILE = Rails.root.join('db', 'order_seeds.csv')
+puts "Loading raw user data from #{ORDER_FILE}"
+
+order_failures = []
+CSV.foreach(ORDER_FILE, :headers => true) do |row|
+  order = Order.new(
+    user_id: row['user_id'],
+    status: row['status'],
+    payment_id: row['payment_id'],
+    address_id: row['address_id'],
+  )
+
+  successful = order.save
+
+  if !successful
+    order_failures << user
+    puts "Failed to save order: #{order.inspect}"
+    puts "#{order.errors.full_messages}"
+  else
+    puts "Created order: #{order.inspect}"
+  end
+end
+
+ORDER_PRODUCTS_FILE = Rails.root.join('db', 'order_products.csv')
+puts "Loading raw user data from #{ORDER_PRODUCTS_FILE}"
+
+order_products_failures = []
+CSV.foreach(ORDER_PRODUCTS_FILE, :headers => true) do |row|
+  order_products = Order_products.new(
+    order_id: row['order_id'],
+    product_id: row['product_id'],
+    quantity: row['quantity']
+  )
+
+  successful = order_products.save
+
+  if !successful
+    order_products_failures << order_products
+    puts "Failed to save order_products: #{order_products.inspect}"
+    puts "#{order_products.errors.full_messages}"
+  else
+    puts "Created order_products: #{order_products.inspect}"
+  end
+
+
+
+  puts "Added #{User.count} user records"
+  puts "#{user_failures.length} users failed to save."
+
+  puts "Added #{Product.count} product records"
+  puts "#{product_failures.length} products failed to save."
+end
