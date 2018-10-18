8b6710e1d611a86a65f6ac8e27e862ae39e59707
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-17 23:13:49 -0700

    something weird with the orders fixtures

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,10 @@ class Order < ApplicationRecord
   has_many :products, through: :order_products
   has_one :payment
   has_one :address
-  has_one :user
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :address_id, presence: true
+  validates :payment_id, presence: true
+  validates :status, presence: true
 end
