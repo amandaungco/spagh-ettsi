2d07955467dbbe76b1e28ab3e30ac37b1ade9bc1
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-19 14:43:49 -0700

    oauth catchup again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-
+  get "/auth/:provider/callback", to: "sessions#create"
   get '/pages/contact_us', to: 'pages#contact_us', as: 'contact_us'
 
   get '/shopping_cart', to: 'orders#shopping_cart', as: 'shopping_cart'
