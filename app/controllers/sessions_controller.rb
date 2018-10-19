c0079b8611cc2551876eff46aaeb1a35db7181b2
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-19 14:10:20 -0700

    updated controller for oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
   def create
-  end
+     auth_hash = request.env['omniauth.auth']
+     user = User.find_by(uid: auth_hash[:uid], provider:'github')
+     raise
+   end
+ end
 
   def destroy
-  end 
+  end
 end
