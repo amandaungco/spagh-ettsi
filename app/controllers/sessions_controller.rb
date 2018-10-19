d82f0f59a1217e547105c2d11ecde30fdba9ff4f
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-19 15:09:51 -0700

    updated sessions controller/navbar/login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,31 @@
 class SessionsController < ApplicationController
+
   def create
+    auth_hash = request.env['omniauth.auth']
+    user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    if user
+      # User was found in the database
+      flash[:success] = "Logged in as returning user #{user.name}"
+    else
+      user = User.build_from_github(auth_hash)
+      if user.save
+        flash[:result_text] = "Logged in as new user #{user.name}"
+      else
+        flash[:result_text] = "Could not create new user account: #{user.errors.messages}"
+        flash[:messages] = user.errors.messages
+        redirect_to root_path
+        return
+      end
+    end
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def destroy
-  end 
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out!"
+
+    redirect_to root_path
+  end
+
 end
