aec290056e8f51e9364b408b7adc07e7ea84921a
Xtina <2006peacegypsy@gmail.com>
2018-10-20 13:05:49 -0700

    setting up authorization and authentication tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe SessionsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "should get login" do
+    get sessions_login_url
+    value(response).must_be :success?
+  end
+
+  it "should get new" do
+    get sessions_new_url
+    value(response).must_be :success?
+  end
+
+  it "should get destroy" do
+    get sessions_destroy_url
+    value(response).must_be :success?
+  end
 end
