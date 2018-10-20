6dbe8deb07b8e3f868b4cc953631a3187565926a
Daniela Di Iorio <danielad@gmail.com>
2018-10-20 16:41:03 -0700

    skeleton order products controller tests

--- test/controllers/order_products_controller_test.rb
+++ test/controllers/order_products_controller_test.rb
@@ -14,6 +14,42 @@ describe OrderProductsController do
 
     end
 
+    it 'modifies an existing row if the product/order combo is already in the table' do
+    end
+
+    it 'fails with invalid data' do
+    end
+
+    it 'decrements the quantity in the product model appropriately' do
+    end
+
+  end
+
+  describe 'update' do
+    it 'modifies an existing row given a valid id' do
+    end
+
+    it 'fails with invalid id' do
+    end
+
+    it 'deletes row if quantity is set to zero' do
+    end
+  end
+
+  describe 'check_login' do
+    it 'does nothing if a user is already logged in' do
+    end
+
+    it 'creates a new guest user instance and logs the guest user in' do
+    end
+  end
+
+  describe 'check_shopping_cart' do
+    it 'sets the session shopping cart id if the user has an order with shopping_cart status' do
+    end
+
+    it 'creates a new shopping cart if the user does not have one and sets the session shopping cart id' do
+    end
   end
 
 
