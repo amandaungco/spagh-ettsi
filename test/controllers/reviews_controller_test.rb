ef0dca3e42e371ace4c6cc66e3ef1a2d16937256
Xtina <2006peacegypsy@gmail.com>
2018-10-22 16:50:36 -0700

    added one test, passing, second test is not passing yet

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
-
+require "pry"
 describe ReviewsController do
   it 'succeeds' do
     get reviews_path
 
     must_respond_with :success
+  end
+
+  it "should show all the reviews" do
+    get reviews_index_url
+    value(response).must_be :success?
+  end
+
+
+  it "should create a new review with valid fields" do
+
   end
 end
