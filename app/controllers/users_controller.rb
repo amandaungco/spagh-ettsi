47947782d4b57038b0cd58bb41a8ffc60e399a5e
Daniela Di Iorio <danielad@gmail.com>
2018-10-22 21:01:23 -0700

    deactivates products when seller goes inactive, pages only show active pasta

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,15 @@ class UsersController < ApplicationController
   def update
     @login_user.is_a_seller = params[:user][:is_a_seller]
     if @login_user.save
+
+      if @login_user.products.any?
+        if @login_user.is_a_seller
+          activate_user_products
+        else
+          deactivate_user_products
+        end
+      end
+
       flash[:success] = "Account settings updated!"
       redirect_to account_path
     else
@@ -21,6 +30,20 @@ class UsersController < ApplicationController
       flash[:validation_errors] = @login_user.errors.full_messages
     end
 
+  end
+
+  def deactivate_user_products
+    @login_user.products.each do |product|
+      product.is_active = false
+      product.save
+    end
+  end
+
+  def activate_user_products
+    @login_user.products.each do |product|
+      product.is_active = true
+      product.save
+    end
   end
 
 end
