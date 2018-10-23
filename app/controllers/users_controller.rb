e860ec3f5f4e6a0a2d41de4f1f8cbfd3672de4cb
Daniela Di Iorio <danielad@gmail.com>
2018-10-22 20:32:47 -0700

    change merchant status on account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,16 @@ class UsersController < ApplicationController
     render :account
   end
 
+  def update
+    @login_user.is_a_seller = params[:user][:is_a_seller]
+    if @login_user.save
+      flash[:success] = "Account settings updated!"
+      redirect_to account_path
+    else
+      flash[:warning] = "Oops, there was a problem updating your account."
+      flash[:validation_errors] = @login_user.errors.full_messages
+    end
+
+  end
+
 end
