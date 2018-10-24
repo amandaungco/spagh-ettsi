d618e938de70c5bafd2ed057c79ae6efe079a6e3
Xtina <2006peacegypsy@gmail.com>
2018-10-23 22:02:00 -0700

    seven of eight tests passing

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -7,26 +7,29 @@ describe ReviewsController do
 
     must_respond_with :success
   end
+  describe "index" do
+    it "index action should show all the reviews" do
+      @reviews = Review.all
 
-  it "index action should show all the reviews" do
-    @reviews = Review.all
+      get reviews_url
+      value(response).must_be :successful?
+    end
 
-    get reviews_url
-    value(response).must_be :successful?
+    it "should display no reviews when there are no reviews" do
+      Review.destroy_all
+
+      get reviews_path
+      must_respond_with :success
+      expect(Review.all.count).must_equal 0
+    end
   end
+  describe "new" do
+    it "succeeds" do
 
-  it "should display no reviews when there are no reviews" do
-    Review.destroy_all
+      get new_review_path
 
-    get reviews_path
-    must_respond_with :success
-    expect(Review.all.count).must_equal 0
-  end
-
-  it "should create a new review" do
-    get new_review_path
-
-    must_respond_with :success
+      must_respond_with :success
+    end
   end
 
   describe "create" do
@@ -51,6 +54,54 @@ describe ReviewsController do
       }.must_change 'Review.count', 1
 
       must_redirect_to reviews_path
+    end
+
+    it "renders bad_request when invalid data is provided" do
+
+      perform_login(users(:buyer))
+      # Arranges
+      review_hash[:review][:rating] = nil
+
+      # Act-Assert
+      expect {
+        post reviews_path, params: review_hash
+      }.wont_change 'Review.count'
+
+      must_respond_with :bad_request
+    end
+  end
+
+  describe "show" do
+    it "succeeds for an existing review" do
+      # Arrange
+      id = reviews(:one).id
+
+      # Act
+      get review_path(id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a non-existing review" do
+
+      let(:review_hash) do
+        {
+          review: {
+            review: 'relieves anxiety',
+            rating: 5,
+            product_id: product.id
+          }
+        }
+      end
+      # Arrange - invalid id
+      id = review_hash[:review][:product_id] = nil
+
+      # Act
+      get review_path(id)
+
+      # Assert
+      must_respond_with :not_found
     end
   end
 end
