967b7f38c81ed0d23507d90808b14bc249674a6a
Xtina <2006peacegypsy@gmail.com>
2018-10-23 20:54:08 -0700

    added avg rating method to reviews model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Review < ApplicationRecord
   belongs_to :product
   belongs_to :user
@@ -5,4 +6,16 @@ class Review < ApplicationRecord
   validates :user_id, presence: true
   validates :product_id, presence: true
   validates :rating, presence: true, numericality: true
+end
+
+def average_rating
+  ratings = 0
+  self.reviews.each do |review|
+    ratings += review.rating if review.rating
+  end
+  if ratings == 0
+    return "Not yet rated"
+  else
+    return (ratings.to_i / self.reviews.length).round(2)
+  end
 end
