4966d90910470680a219e33409a453f978dbaf67
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-25 10:59:29 -0700

    fixed rating review

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
 
   validates :user_id, presence: true
   validates :product_id, presence: true
-  validates :rating, presence: true, numericality: { greater_than: 1, message: "Rating must be greater than 0"}
+  validates :rating, presence: true, numericality: { greater_than: 0, message: "Rating must be greater than 0"}
   validates :rating, numericality: { less_than_or_equal_to: 5, message: "We're flattered but this product can only be rated out of 5."}
   # def item_ownership?
   #   return self.product.user_id == session[:user_id]
