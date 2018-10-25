adf11d129431140f1d5d60e970790076db7357fd
Xtina <2006peacegypsy@gmail.com>
2018-10-25 13:55:54 -0700

    fixing html error

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,6 +22,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user = current_user
+    binding.pry
       if @review.save
         flash[:success] = "Thanks for creating a review!"
 
